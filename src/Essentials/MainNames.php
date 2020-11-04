6c00a6b120402b0c9c37298a403329edc87cb23b
Tomas Johanovsky <tjohanovsky306@gmail.com>
2020-11-04 13:57:23 +0100

    Add new images, scss file, controllers, name file atc.

---
+++ src/Essentials/MainNames.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Essentials;
+
+
+class MainNames
+{
+    const OFFICIAL_NAME = 'Základní a Mateřská škola Osečná';
+    const FULL_NAME = 'Základní škola a Mateřská škola, Osečná, okres Liberec, příspěvková organizace';
+    const PRIMARY_SCHOOL = 'Základní škola';
+    const KINDER_GARDEN = 'Mateřská škola';
+    const SCHOOL_CLUB = 'Školní družina';
+    const RENTALS = 'Pronájmy';
+    const SKOLA_ONLINE = 'Škola OnLine';
+    const NASE_MS = 'Naše MŠ';
+}
